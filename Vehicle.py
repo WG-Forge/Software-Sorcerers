8fece6ecc4d34e3402bf531934edf3de017ca91f
DimonDimskiy <skdmyr@gmail.com>
2023-03-29 22:35:12 +0200

    Upd Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -1,16 +1,23 @@
 from typing import Optional
+
+
 import cube_math as cm
+from Model import TankModel
+
+MT_MAX_RANGE = 2
+MT_MIN_RANGE = 1
+MT_SPEED_POINTS = 2
 
 
 class VehicleFactory:
     @staticmethod
-    def build(spec: tuple[int, tuple[tuple[int, int, int], str]]) -> "Vehicle":
+    def build(spec: tuple[int, "TankModel"]) -> "Vehicle":
         """
         instantiates vehicles of given spec
         :param spec: tuple(vehicle_id (coordinates, vehicle_type))
         :return: Vehicle of proper type
         """
-        match spec[-1][-1]:
+        match spec[-1].vehicle_type:
             case "medium_tank":
                 return MediumTank(spec)
             case "light_tank":
@@ -24,79 +31,64 @@ class VehicleFactory:
 
 
 class Vehicle:
-    def __init__(self, id: int, coordinates: tuple[int, int, int], hp: int, sp: int, shoot_distance: int,):
-        self.id
-        self.coordinates = coordinates
-        self.hp = hp
+    def __init__(self,  spec: tuple[int, "TankModel"], sp: int, max_range: int, min_range: int):
+        self.id = spec[0]
+        self.model = spec[-1]
         self.sp = sp
-        self.shoot_distance = shoot_distance
-        #self.capture_points = 0 #maybe needed
-        #self.destruction_points = 0 #maybe needed
+        self.max_shoot_range = max_range
+        self.min_range = min_range
+        self.priority = None
 
-    def make_turn(self, state, map_, actions) -> Optional[tuple[str, dict]]:
-        # must return ("SHOOT", {dict}), ("MOVE", {dict}), or None
+    def refresh_model(self, state):
+        self.model = state.our_tanks[self.id]
 
-        self.refresh_state(state)
+    def cells_in_range(self):
+        return cm.in_radius_excl(self.model.coordinates, self.min_range, self.max_shoot_range
+                                 + self.model.shoot_range_bonus)
 
-        can_shoot, coord = self.can_shoot(state, map_, actions)
+    def targets_in_range(self, state):
+        return self.cells_in_range().intersection(state.agressive_cells)
 
-        if can_shoot:
-            return "SHOOT", {"vehicle_id":id,"target":{"x":coord[0],"y":coord[1],"z":coord[2]}}
-        
-        can_move, coord = self.can_move(state, map_, actions)
+    def make_turn(self, state, map_) -> Optional[tuple[str, dict]]:
+        self.refresh_model(state)
+        targets = self.targets_in_range(state)
+        if targets:
+            # TODO Shooting strategy is here, which target we choose, probably first we have to shoot targets on base, after that targets with  low hp
+            target = targets.pop()
+            return "SHOOT", {"vehicle_id": self.id, "target": {"x": target[0], "y": target[1], "z": target[2]}}
+        self.set_priority(state, map_)
+        if self.priority():
+            return self.move_to_priority(map_, state)
 
-        if can_move:
-            return "MOVE", {"vehicle_id":id,"target":{"x":coord[0],"y":coord[1],"z":coord[2]}}
+    def set_priority(self, state, map_):
+        is_self_in_base = self.model.coordinates in map_.base_cells
+        if is_self_in_base:
+            self.priority = None
+        our_tanks_in_base = state.our_tanks_cells.intersect(map_.base_cells)
+        empty_base_cells = map_.base_cells.difference(state.tank_cells)
+        if empty_base_cells and our_tanks_in_base < 2:
+            self.priority = empty_base_cells.pop(0)
+            # TODO: all strategy is here, should set priority (one free cell or None), we can overload it for different types of vehicles
 
-        return None
-    
-    def refresh_state(self, state):
-        #updates vehicle coordinates and hp
-
-        self.coordinates = state.vehicles[self.id]["position"]
-        self.hp = state.vehicles[self.id]["health"]
-
-    def can_shoot(self, state, map_, actions) -> tuple[bool, Optional[tuple[int, int, int]]]:
-        # must return (True, (x, y, z)) or (False, None)
-
-        for vehicle_id, vehicle in state.vehicles.items():
-
-            #we're not gonna shoot our tanks
-            if state.current_player_idx == vehicle["player_id"]:
-                continue
-            
-            if cm.distance(self.coordinates, vehicle["position"]) != self.shoot_distance:
-                continue
-
-            #TODO: check if we are able to shoot this tank (rule of neutrality)
-            
-        #if we didn't find any enemy tanks in range return False, None
-        return False, None
-
-    def can_move(self, state, map_, actions) -> tuple[bool, Optional[tuple[int, int, int]]]:
-        # must return (True, (x, y, z)) or (False, None)
-
-        #if tank is in base, it shouldn't move
-        if self.coordinate in map_.base:
-            return False, None
-        
-        #if tank is not in base, try to move towards the base
-        for base_coord in map_.base:
-            path = cm.a_star(map_.cells, self.coordinates, base_coord) 
-            if path[1] in map_.available_cells:
-                return True, path[1]
-            elif path[0] in map_.available_cells:
-                return True, path[0]
-            else:
-                return False, None
+    def move_to_priority(self, map_, state):
+        step_cell = None
+        path_to_priority = cm.a_star(map_.available_calls, self.model.coordinates, self.priority)
+        speed_points = self.sp
+        if speed_points >= len(path_to_priority):
+            step_cell = path_to_priority[-1]
+        else:
+            while speed_points:
+                if not(path_to_priority[speed_points - 1] in state.tank_cells):
+                    step_cell = path_to_priority[speed_points - 1]
+                    break
+                speed_points -= 1
+        if step_cell:
+            return "MOVE", {"vehicle_id": self.id, "target": {"x": step_cell[0], "y": step_cell[1], "z": step_cell[2]}}
 
 
 class MediumTank(Vehicle):
-    def __init__(self, spec: tuple[int, tuple[tuple[int, int, int], str]]):
-        """
-        :param spec: tuple(vehicle_id (coordinates, vehicle_type))
-        """
-        super().__init__(spec[0], spec[1][0], 2, 2, 2)
+    def __init__(self, spec: tuple[int, "TankModel"]):
+        super().__init__(spec, MT_SPEED_POINTS, MT_MAX_RANGE, MT_MIN_RANGE)
 
 # <------------- End of stage 1
 
