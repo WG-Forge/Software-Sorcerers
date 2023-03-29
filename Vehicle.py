56eb4577977660b7c7964a568ef35192a71bca28
DimonDimskiy <skdmyr@gmail.com>
2023-03-30 01:12:46 +0200

    Upd Vehicle.py 2

--- Vehicle.py
+++ Vehicle.py
@@ -1,8 +1,8 @@
-from typing import Optional
+from typing import Optional, Union
 
 
 import cube_math as cm
-from Model import TankModel
+from Model import TankModel, GameState, GameMap
 
 MT_MAX_RANGE = 2
 MT_MIN_RANGE = 1
@@ -39,30 +39,39 @@ class Vehicle:
         self.min_range = min_range
         self.priority = None
 
-    def refresh_model(self, state):
+    def refresh_model(self, state: "GameState") -> None:
         self.model = state.our_tanks[self.id]
 
-    def cells_in_range(self):
+    def cells_in_range(self) -> set[tuple[int, int, int]]:
         return cm.in_radius_excl(self.model.coordinates, self.min_range, self.max_shoot_range
                                  + self.model.shoot_range_bonus)
 
-    def targets_in_range(self, state):
+    def targets_in_range(self, state: "GameState") -> Union[set, set[tuple[int, int, int]]]:
         return self.cells_in_range().intersection(state.agressive_cells)
+
+    def shoot(self, target: tuple[int, int, int]) -> tuple[str, dict]:
+        return "SHOOT", {"vehicle_id": self.id, "target": {"x": target[0], "y": target[1], "z": target[2]}}
+
+    def move(self, cell: tuple[int, int, int]) -> tuple[str, dict]:
+        return "MOVE", {"vehicle_id": self.id, "target": {"x": cell[0], "y": cell[1], "z": cell[2]}}
+
+    def choose_target(self, targets: set[tuple[int, int, int]], state: "GameState", map_: "GameMap")\
+            -> tuple[int, int, int]:
+        return targets.pop()  # TODO here some target choosing logic, targets are already shootable:)
 
     def make_turn(self, state, map_) -> Optional[tuple[str, dict]]:
         self.refresh_model(state)
         targets = self.targets_in_range(state)
         if targets:
-            # TODO Shooting strategy is here, which target we choose, probably first we have to shoot targets on base, after that targets with  low hp
-            target = targets.pop()
-            return "SHOOT", {"vehicle_id": self.id, "target": {"x": target[0], "y": target[1], "z": target[2]}}
+            target = self.choose_target(targets, state, map_)
+            return self.shoot(target)
         self.set_priority(state, map_)
         if self.priority():
             return self.move_to_priority(map_, state)
 
     def set_priority(self, state, map_):
-        is_self_in_base = self.model.coordinates in map_.base_cells
-        if is_self_in_base:
+        is_in_base = self.model.coordinates in map_.base_cells
+        if is_in_base:
             self.priority = None
         our_tanks_in_base = state.our_tanks_cells.intersect(map_.base_cells)
         empty_base_cells = map_.base_cells.difference(state.tank_cells)
@@ -83,7 +92,7 @@ class Vehicle:
                     break
                 speed_points -= 1
         if step_cell:
-            return "MOVE", {"vehicle_id": self.id, "target": {"x": step_cell[0], "y": step_cell[1], "z": step_cell[2]}}
+            return self.move(step_cell)
 
 
 class MediumTank(Vehicle):
