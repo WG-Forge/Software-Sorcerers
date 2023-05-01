098154ecf44d4fcf41883d1e0bd15ae001c29ad9
dimondimskiy <skdmyr@gmail.com>
2023-05-02 01:31:28 +0200

    add CONTENT_FILL

--- GUI/ui.py
+++ GUI/ui.py
@@ -5,10 +5,15 @@ This module contains constants using in GUI
 HEX_BORDER_COLOR = (0, 0, 0)
 HEX_BORDER_WEIGHT = 2
 HEX_DEFAULT_FILL = (200, 200, 200)
-OBSTACLE_COLOR = (64, 64, 64)
-BASE_COLOR = (255, 204, 153)
 OUR_TANKS_COLOR = (178, 255, 102)
 ENEMY_COLOR = (255, 102, 102)
-SPAWN_COLOR = (160, 160, 160)
 SCREEN_TO_WINDOW_HIGH_RATIO = 1.1
 SCREEN_TO_WINDOW_WIDTH_RATIO = 2
+CONTENT_FILL = {
+    "obstacle": ("", (64, 64, 64)),
+    "base": ("", (255, 204, 153)),
+    "catapult": ("CP", (128, 135, 102)),
+    "hard_repair": ("HR",(128, 135, 102)),
+    "light_repair": ("LR", (128, 135, 102)),
+    "spawn": ("", (160, 160, 160))
+}
