3562fccfc0a9ede4bef50d96906819a004234293
dimondimskiy <skdmyr@gmail.com>
2023-05-02 01:30:03 +0200

    moved multiplayer to tests

--- main.py
+++ main.py
@@ -4,41 +4,18 @@ This module is an entry point to the app.
 from PySide6 import QtWidgets
 
 from GUI.main_window import Window
-from logic.game import Game
 
-login_data_1 = {
+login_data = {
     "name": "Sorcerer1",
-    "password": "36",
-    "game": "my2121",
+    "password": "my_pass",
+    "game": "my_game",
     "num_turns": 45,
-    "num_players": 3,
-    "is_observer": False,
-}
-
-login_data_2 = {
-    "name": "Sorcerer2",
-    "password": "36",
-    "game": "my2121",
-    "num_turns": 45,
-    "num_players": 3,
-    "is_observer": False,
-}
-
-login_data_3 = {
-    "name": "Sorcerer3",
-    "password": "36",
-    "game": "my2121",
-    "num_turns": 45,
-    "num_players": 3,
+    "num_players": 1,
     "is_observer": False,
 }
 
 if __name__ == "__main__":
-    player2 = Game(login_data_2)
-    player2.start()
-    player3 = Game(login_data_3)
-    player3.start()
     app = QtWidgets.QApplication()
-    window = Window(login_data_1)
+    window = Window(login_data)
     window.show()
     app.exec()
