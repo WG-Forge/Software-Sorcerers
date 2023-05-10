94cd7ce6b382d6fe60300fdd6dcd66d0569f3d69
DimonDimskiy <skdmyr@gmail.com>
2023-05-10 08:06:41 +0200

    entry point modified according to new login window

--- main.py
+++ main.py
@@ -3,20 +3,11 @@ This module is an entry point to the app.
 """
 from PySide6 import QtWidgets
 
-from GUI.main_window import Window
+from GUI.login_window import LoginWindow
 
-login_data = {
-    "name": "Sorcerer22",
-    "password": "42",
-    "game": "test_122",
-    "num_turns": 45,
-    "num_players": 1,
-    "is_observer": False,
-    "is_full": False
-}
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
-    window = Window(login_data)
+    window = LoginWindow()
     window.show()
     app.exec()
